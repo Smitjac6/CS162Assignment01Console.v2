8c45394bb6cfe2abfe00cd6e0c1c3aa4734d9ca0
Jacob Smith <smitjac6@oregonstate,edu>
2024-10-05 11:52:27 -0700

    takes user input

--- IOconsole.java
+++ IOconsole.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class IOconsole {
     public static void main (String []args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your Favorite drink?");
+        String drink = scanner.nextLine(); 
         
+        System.out.println("You like " + drink);
+
         if (args.length > 0) {
-            // Print the first argument
-            System.out.println("The first argument is: " + args[0]);
+            
+            System.out.println("You like " + args[0]);
         }
-        else{
-        System.out.println( "Oh haiiiii");
-        System.out.println( "Hi again!"); }
-    }
+        
+        }
 }
+
