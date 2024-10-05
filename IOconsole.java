446a8b65a90e1777565dfcc7f884ada2c756546a
Jacob Smith <smitjac6@oregonstate,edu>
2024-10-05 11:31:14 -0700

    commit 3

--- IOconsole.java
+++ IOconsole.java
@@ -1,6 +1,12 @@
 public class IOconsole {
     public static void main (String []args){
+        
+        if (args.length > 0) {
+            // Print the first argument
+            System.out.println("The first argument is: " + args[0]);
+        }
+        else{
         System.out.println( "Oh haiiiii");
-        System.out.println( "Hi again!");
+        System.out.println( "Hi again!"); }
     }
 }
